dcf6dad282c7f655fc323f5ed58e0c3dac87b2cc
Junju Ng <ngjunju@outlook.com>
2019-02-27 15:42:56 -0500

    modify studentServer.js for cross origin requests

--- studentServer.js
+++ studentServer.js
@@ -26,6 +26,12 @@ app.use(function(req, res, next){
 	next();
 });
 
-// serve statis files - e.g. html, css
+app.use(function(req, res, next){
+	res.header("Access-Control-Allow-Origin","*");
+	res.header("Access-Control-Allow-Headers","X-Requested-With");
+	next();
+});
+
+// serve static files - e.g. html, css
 // should be the last line in the server
 app.use(express.static(__dirname));
