7c9af339d9fcce49eb396585d153a2ad087511de
Junju Ng <ngjunju@outlook.com>
2019-03-03 14:17:23 -0500

    add get functionality to generate GeoJSON from any spatial table

--- studentServer.js
+++ studentServer.js
@@ -135,6 +135,70 @@ app.get('/getFormData/:port_id', function (req, res) {
     });
 });
 	
+// make geojson server more flexible to generate geojson from any spatial table
+app.get('/getGeoJSON/:tablename/:geomcolumn/:portNumber?', function(req,res){
+	pool.connect(function(err,client,done){
+		// if unable to connect
+		if (err){
+			console.log("not able to get connection " + err);
+			res.status(400).send(err);
+		}
+		
+		var colnames = "";
+		// get list of columns in the table
+		// use string_agg to generate csv list that can then be pasted into next query
+		 var tablename = req.params.tablename;
+        var geomcolumn = req.params.geomcolumn;
+        var querystring = "select string_agg(colname,',') from ( select column_name as colname ";
+        querystring = querystring + " FROM information_schema.columns as colname ";
+        querystring = querystring + " where table_name   =$1";
+        querystring = querystring + " and column_name <> $2 and data_type <> 'USER-DEFINED') as cols ";
+		console.log("Query string 1: " + querystring);
+		
+		// run the query
+		client.query(querystring,[tablename, geomcolumn], function(err,result){
+			done(); // call done() to release client back to the pool
+			// raise error
+			if (err){
+				console.log(err);
+				res.status(400).send(err);
+			}
+			thecolnames = result.rows[0].string_agg;
+			colnames = thecolnames;
+			console.log("the colnames " + thecolnames); // log colnames in console
+			
+			// use the inbuilt geoJSON functionality
+			// and create the required geoJSON format using a query adapted from here: http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018
+			// note that query needs to be a single string with no line breaks so built it up bit by bit
+			var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features  FROM ";
+            querystring = querystring + "(SELECT 'Feature' As type     , ST_AsGeoJSON(lg." + req.params.geomcolumn + ")::json As geometry, ";
+            querystring = querystring + "row_to_json((SELECT l FROM (SELECT " + colnames + ") As l      )) As properties";
+
+			// depending on whether we have a port number, do different things
+			// if we have a portNumber
+			if (req.params.portNumber) {
+                querystring = querystring + "   FROM " + req.params.tablename + "As lg where lg.port_id = '" + req.params.portNumber + "' limit 100  ) As f ";
+            }
+			else {
+                querystring = querystring + "    FROM " + req.params.tablename + " As lg limit 100 ) As f ";
+            }
+
+			console.log("Query string 2: " + querystring);
+			
+			// run the 2nd query
+			client.query(querystring, function(err, result){
+				done(); // release client back to the pool
+				if (err){
+					console.log(err);
+					res.status(400).send(err);
+				}
+				res.status(200).send(result.rows);
+			});
+		});
+	});
+});
+				
+
 
 
 
